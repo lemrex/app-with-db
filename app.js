c73e2cdeb5fbe9503f33512e5c63828345aaff5f
Raphael <95745431+lemrex@users.noreply.github.com>
2023-12-19 14:19:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
       <body>
         <h2>Enter a Name</h2>
         <form action="/addName" method="post">
-          <input type="text" name="name" placeholder="Enter a name" required>
+          <input type="text" name="name" placeholder="Enter your name" required>
           <button type="submit">Submit</button>
         </form>
       </body>
